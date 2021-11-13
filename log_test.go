9d78436934a91281b32b91f51413df1088f07bbd
ddddddO <lbfdeatq@gmail.com>
2021-11-13 23:13:45 +0900

    テーブル形式のテストへ

--- log_test.go
+++ log_test.go
@@ -9,14 +9,32 @@ func TestLogger_Debug(t *testing.T) {
 	buf := &bytes.Buffer{}
 	log := New(buf)
 
-	log.WithPlain()
+	tests := []struct {
+		name     string
+		modeFunc func()
+		in       map[string]interface{}
+		want     string
+	}{
+		{
+			name:     "output plain",
+			modeFunc: log.WithPlain,
+			in: map[string]interface{}{
+				"message": "debug for test",
+				"name":    "ddddd",
+				"id":      1111,
+			},
+			want: "\x1b[33mDEBUG\x1b[0m message:debug for test name:ddddd id:1111\n",
+		},
+	}
 
-	log.Debug("debug for test").Str("name", "ddddd").Int("id", 1111).Output()
-	got := buf.String()
-	want := "\x1b[33mDEBUG\x1b[0m message:debug for test name:ddddd id:1111\n"
+	for _, tt := range tests {
+		tt.modeFunc()
+		log.Debug(tt.in["message"].(string)).Str("name", tt.in["name"].(string)).Int("id", tt.in["id"].(int)).Output()
+		got := buf.String()
 
-	if got != want {
-		t.Errorf("got:%s\nwant:%s", got, want)
+		if got != tt.want {
+			t.Errorf("got:%s\nwant:%s", got, tt.want)
+		}
 	}
 }
 
@@ -24,13 +42,31 @@ func TestLogger_Info(t *testing.T) {
 	buf := &bytes.Buffer{}
 	log := New(buf)
 
-	log.WithPlain()
+	tests := []struct {
+		name     string
+		modeFunc func()
+		in       map[string]interface{}
+		want     string
+	}{
+		{
+			name:     "output plain",
+			modeFunc: log.WithPlain,
+			in: map[string]interface{}{
+				"message": "info for test",
+				"name":    "aaaaa",
+				"id":      22222,
+			},
+			want: "\x1b[34mINFO\x1b[0m message:info for test name:aaaaa id:22222\n",
+		},
+	}
 
-	log.Info("info for test").Str("name", "aaaaa").Int("id", 22222).Output()
-	got := buf.String()
-	want := "\x1b[34mINFO\x1b[0m message:info for test name:aaaaa id:22222\n"
+	for _, tt := range tests {
+		tt.modeFunc()
+		log.Info(tt.in["message"].(string)).Str("name", tt.in["name"].(string)).Int("id", tt.in["id"].(int)).Output()
+		got := buf.String()
 
-	if got != want {
-		t.Errorf("got:%s\nwant:%s", got, want)
+		if got != tt.want {
+			t.Errorf("got:%s\nwant:%s", got, tt.want)
+		}
 	}
 }
