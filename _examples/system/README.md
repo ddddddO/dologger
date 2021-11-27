e46de4ed46196a7d6a5aeb430c76570abce54a38
ddddddO <lbfdeatq@gmail.com>
2021-11-27 17:43:26 +0900

    fluent bitへログを送ってみた

---
+++ _examples/system/README.md
@@ -0,0 +1,20 @@
+dologger (on Application) -> Fluent Bit -> ??? のサンプルコード
+
+
+# Usage
+
+1. Fluent Bitをtcpモードで起動
+
+```console
+make fbd
+```
+
+2. 別ターミナルでFluent Bitへログを送出するappを起動
+
+```console
+make app
+```
+
+# References
+
+- https://github.com/ddddddO/dologger/issues/3#issue-1064890682
---
+++ _examples/system/app/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/ddddddO/dologger"
+)
+
+const (
+	interval = 2 * time.Second
+
+	protocol   = "tcp"
+	fluentHost = "localhost"
+	fluentPort = 5170
+)
+
+func main() {
+	fmt.Println("start sample app")
+
+	conn, err := net.Dial(
+		protocol,
+		fmt.Sprintf("%s:%d", fluentHost, fluentPort),
+	)
+	if err != nil {
+		panic(err)
+	}
+	log := dologger.New(conn)
+	log.WithJSON()
+
+	ticker := time.NewTicker(interval)
+	for t := range ticker.C {
+		fmt.Println("send log")
+
+		// receive_logプロセスへTCPでログを送る
+		log.Debug("for debug").
+			Str("name", "ddddd").
+			Int("second", t.Second()).
+			Out()
+	}
+}
